05d881bfe3112796db8aed7dc0f85f840540891d
Lucas <63049317+teixeiradlucas@users.noreply.github.com>
2023-12-27 16:25:05 -0300

    Criado homeview

--- lib/home/home_view.dart
+++ lib/home/home_view.dart
@@ -1,3 +1,4 @@
+import 'package:app_pc_store/constants/strings/strings_generic.dart';
 import 'package:flutter/material.dart';
 
 class HomeView extends StatelessWidget {
@@ -5,6 +6,40 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(
+          child: Text(AppStringsGeneric.appName),
+        ),
+        leading: IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.menu),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.shop_2_rounded),
+          ),
+        ],
+      ),
+      body: ListView(
+        children: [
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Monte seu pc'),
+          ),
+          Row(
+            children: [
+              TextButton(onPressed: () {}, child: const Text('Processador')),
+              TextButton(onPressed: () {}, child: const Text('Placa mãe')),
+              TextButton(onPressed: () {}, child: const Text('Placa de vídeo')),
+              TextButton(onPressed: () {}, child: const Text('Memória ram')),
+              TextButton(onPressed: () {}, child: const Text('Gabinete')),
+              TextButton(onPressed: () {}, child: const Text('Processador')),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
