fe9477785c691827b7a4d1d1a66cf875b92568d8
Lucas <63049317+teixeiradlucas@users.noreply.github.com>
2023-12-27 14:11:13 -0300

    Initial comment

---
+++ android/app/src/main/kotlin/com/example/app_pc_store/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.app_pc_store
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:app_pc_store/home/home_view.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomeView(),
+    );
+  }
+}
