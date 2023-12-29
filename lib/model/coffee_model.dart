b924c3cb58b3669e30df662bda6d564b2cef1aaa
Lucas <63049317+teixeiradlucas@users.noreply.github.com>
2023-12-29 06:47:51 -0300

    Implementando objetos

---
+++ lib/model/coffee_model.dart
@@ -0,0 +1,135 @@
+class Coffee {
+  Coffee({
+    required this.name,
+    required this.intensity,
+    required this.beverageType,
+    required this.price,
+  });
+  final String name;
+  final int intensity;
+  final String beverageType;
+  final double price;
+}
+
+List<Coffee> coffees = [
+  Coffee(
+    name: 'Espresso 1',
+    intensity: 5,
+    beverageType: 'Espresso',
+    price: 3.50,
+  ),
+  Coffee(
+    name: 'Espresso 2',
+    intensity: 4,
+    beverageType: 'Espresso',
+    price: 4.00,
+  ),
+  Coffee(
+    name: 'Espresso 3',
+    intensity: 5,
+    beverageType: 'Espresso',
+    price: 3.75,
+  ),
+  Coffee(
+    name: 'Espresso 4',
+    intensity: 4,
+    beverageType: 'Espresso',
+    price: 3.90,
+  ),
+  Coffee(
+    name: 'Espresso 5',
+    intensity: 5,
+    beverageType: 'Espresso',
+    price: 4.25,
+  ),
+  Coffee(
+    name: 'Cappuccino 1',
+    intensity: 3,
+    beverageType: 'Cappuccino',
+    price: 5.50,
+  ),
+  Coffee(
+    name: 'Cappuccino 2',
+    intensity: 4,
+    beverageType: 'Cappuccino',
+    price: 6.00,
+  ),
+  Coffee(
+    name: 'Cappuccino 3',
+    intensity: 3,
+    beverageType: 'Cappuccino',
+    price: 5.75,
+  ),
+  Coffee(
+    name: 'Cappuccino 4',
+    intensity: 4,
+    beverageType: 'Cappuccino',
+    price: 6.20,
+  ),
+  Coffee(
+    name: 'Cappuccino 5',
+    intensity: 3,
+    beverageType: 'Cappuccino',
+    price: 5.90,
+  ),
+  Coffee(
+    name: 'Latte 1',
+    intensity: 3,
+    beverageType: 'Latte',
+    price: 6.00,
+  ),
+  Coffee(
+    name: 'Latte 2',
+    intensity: 3,
+    beverageType: 'Latte',
+    price: 6.25,
+  ),
+  Coffee(
+    name: 'Latte 3',
+    intensity: 2,
+    beverageType: 'Latte',
+    price: 5.75,
+  ),
+  Coffee(
+    name: 'Latte 4',
+    intensity: 3,
+    beverageType: 'Latte',
+    price: 6.50,
+  ),
+  Coffee(
+    name: 'Latte 5',
+    intensity: 3,
+    beverageType: 'Latte',
+    price: 6.30,
+  ),
+  Coffee(
+    name: 'Macchiato 1',
+    intensity: 4,
+    beverageType: 'Macchiato',
+    price: 4.50,
+  ),
+  Coffee(
+    name: 'Macchiato 2',
+    intensity: 3,
+    beverageType: 'Macchiato',
+    price: 4.75,
+  ),
+  Coffee(
+    name: 'Macchiato 3',
+    intensity: 4,
+    beverageType: 'Macchiato',
+    price: 4.60,
+  ),
+  Coffee(
+    name: 'Macchiato 4',
+    intensity: 3,
+    beverageType: 'Macchiato',
+    price: 5.00,
+  ),
+  Coffee(
+    name: 'Macchiato 5',
+    intensity: 4,
+    beverageType: 'Macchiato',
+    price: 4.90,
+  ),
+];
