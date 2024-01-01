cccbed6e0fb0a6dae38cf6f1ea41a5e60e5db5b4
Lucas <63049317+teixeiradlucas@users.noreply.github.com>
2024-01-01 14:19:16 -0300

    Adicionado AppText, fonts e colors

---
+++ lib/constants/components/app_text.dart
@@ -0,0 +1,99 @@
+import 'package:app_coffee_shop/constants/themes/app_colors.dart';
+import 'package:app_coffee_shop/constants/themes/app_fonts.dart';
+import 'package:flutter/material.dart';
+
+class AppText extends StatelessWidget {
+  factory AppText.h1(
+    String text, {
+    TextAlign textAlign = TextAlign.start,
+  }) {
+    return AppText._(
+      text,
+      const TextStyle(
+        fontFamily: AppFonts.poppins,
+        fontSize: 20,
+        fontWeight: FontWeight.w700,
+        height: 1.5,
+        color: AppColors.blackColor,
+      ),
+      maxLines: 3,
+      textAlign: textAlign,
+    );
+  }
+  factory AppText.h2(
+    String text, {
+    TextAlign textAlign = TextAlign.start,
+  }) {
+    return AppText._(
+      text,
+      const TextStyle(
+        fontFamily: AppFonts.poppins,
+        fontSize: 20,
+        fontWeight: FontWeight.w700,
+        height: 1.5,
+        color: AppColors.whiteColor,
+      ),
+      maxLines: 3,
+      textAlign: textAlign,
+    );
+  }
+
+  factory AppText.body(
+    String text, {
+    TextAlign textAlign = TextAlign.start,
+  }) {
+    return AppText._(
+      text,
+      const TextStyle(
+        fontFamily: AppFonts.poppins,
+        fontSize: 12,
+        fontWeight: FontWeight.w700,
+        height: 1,
+        color: AppColors.blackColor,
+      ),
+      maxLines: 3,
+      textAlign: textAlign,
+    );
+  }
+
+  factory AppText.descrition(
+    String text, {
+    TextAlign textAlign = TextAlign.start,
+  }) {
+    return AppText._(
+      text,
+      const TextStyle(
+        fontFamily: AppFonts.poppins,
+        fontSize: 10,
+        fontWeight: FontWeight.w700,
+        height: 1,
+        color: Color.fromARGB(255, 80, 80, 80),
+      ),
+      maxLines: 3,
+      textAlign: textAlign,
+    );
+  }
+
+  const AppText._(
+    this.text,
+    this.textStyle, {
+    this.textAlign = TextAlign.left,
+    this.maxLines = 1,
+  });
+
+  final String text;
+  final TextStyle textStyle;
+  final int? maxLines;
+  final TextAlign textAlign;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: textStyle,
+      maxLines: maxLines,
+      textAlign: textAlign,
+      overflow: TextOverflow.ellipsis,
+    );
+  }
+}
--- lib/constants/themes/app_colors.dart
+++ lib/constants/themes/app_colors.dart
@@ -1,11 +1,15 @@
 import 'dart:ui';
 
 class AppColors {
-  static const Color orangeColor = Color.fromARGB(255, 186, 62, 18);
+  static const Color grayColor = Color.fromARGB(255, 155, 155, 155);
 
   static const Color whiteColor = Color.fromARGB(255, 255, 255, 255);
 
+  static const Color beigeColor = Color.fromARGB(255, 187, 185, 185);
+
   static const Color blackColor = Color.fromARGB(255, 0, 0, 0);
 
-  static const Color brownCoffeeColor = Color.fromARGB(255, 71, 2, 2);
+  static const Color brownCoffeeColor = Color(0xFF65422E);
+
+  static const Color backgroundColor = Color(0xFFEAEAEA);
 }
