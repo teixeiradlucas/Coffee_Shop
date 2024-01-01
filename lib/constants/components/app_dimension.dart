01cd6781f1ac81e3d529165efd19deeced8277cd
Lucas <63049317+teixeiradlucas@users.noreply.github.com>
2024-01-01 15:51:31 -0300

    Adicionado novo padding

--- lib/constants/components/app_dimension.dart
+++ lib/constants/components/app_dimension.dart
@@ -13,15 +13,9 @@ class AppDimens {
   // Padding
   static const double kPaddingXS = 2;
   static const double kPaddingS = 4;
+  static const double kPadding = 6;
   static const double kPaddingM = 8;
   static const double kDefaultPadding = 16;
   static const double kPaddingXL = 32;
   static const double kPaddingXXL = 64;
-
-  // Radius and Border
-  static const double defaultRadius = 16;
-  static BorderRadius defaultBorder({double radius = defaultRadius}) =>
-      BorderRadius.circular(radius);
-  static const Offset defaultOffset = Offset(5, 5);
-  static const double defaultBlurRadius = 10;
 }
