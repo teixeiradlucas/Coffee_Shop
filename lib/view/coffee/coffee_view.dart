2f106bb8b4369a8111f8906943464e437a9637c2
Lucas <63049317+teixeiradlucas@users.noreply.github.com>
2024-01-07 17:58:30 -0300

    Criando componentes no CoffeeView

--- lib/view/coffee/coffee_view.dart
+++ lib/view/coffee/coffee_view.dart
@@ -4,6 +4,7 @@ import 'package:app_coffee_shop/constants/themes/app_colors.dart';
 import 'package:app_coffee_shop/model/coffee.dart';
 import 'package:app_coffee_shop/view/home/components/price.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class CoffeeView extends StatelessWidget {
   const CoffeeView({
@@ -22,14 +23,37 @@ class CoffeeView extends StatelessWidget {
         .first;
     final realPrice = finalPrice(itemCoffees);
     return Scaffold(
-      body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            _coffeeContent(size, itemCoffees, context),
-            _buy(realPrice, size),
-          ],
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          _coffeeContent(size, itemCoffees, context),
+          Padding(
+            padding: const EdgeInsets.all(AppDimens.kDefaultPadding),
+            child: CustomText.h1('Tamanho'),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(AppDimens.kDefaultPadding),
+            child: _sizeCoffee(size, Container()),
+          ),
+          _buy(realPrice, size),
+        ],
+      ),
+    );
+  }
+
+  ClipRRect _sizeCoffee(
+    Size size,
+    Widget child, {
+    double padding = AppDimens.kPaddingXXL,
+  }) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(padding),
+      child: Container(
+        color: AppColors.whiteColor,
+        width: size.width,
+        height: 60,
+        child: child,
       ),
     );
   }
@@ -40,31 +64,31 @@ class CoffeeView extends StatelessWidget {
         topLeft: Radius.circular(AppDimens.kPaddingXL),
         topRight: Radius.circular(AppDimens.kPaddingXL),
       ),
-      child: ColoredBox(
+      child: Container(
+        height: size.height * 0.15,
         color: AppColors.whiteColor,
         child: Padding(
-          padding: const EdgeInsets.all(AppDimens.kPaddingXL),
+          padding: const EdgeInsets.all(AppDimens.kPaddingM),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Column(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   CustomText.h2('PREÇO'),
                   CustomText.h1(realPrice),
                 ],
               ),
               ClipRRect(
-                borderRadius: BorderRadius.circular(AppDimens.kPaddingXL),
+                borderRadius: BorderRadius.circular(AppDimens.kDefaultPadding),
                 child: Container(
                   color: AppColors.brownCoffeeColor,
-                  height: size.height * 0.10,
-                  child: Padding(
-                    padding: const EdgeInsets.all(AppDimens.kPaddingXL),
-                    child: Center(
-                      child: CustomText.h2(
-                        'Adicionar ao carrinho',
-                        color: AppColors.whiteColor,
-                      ),
+                  height: size.height * 0.08,
+                  width: size.width * 0.6,
+                  child: Center(
+                    child: CustomText.h2(
+                      'Adicionar ao carrinho',
+                      color: AppColors.whiteColor,
                     ),
                   ),
                 ),
@@ -105,56 +129,43 @@ class CoffeeView extends StatelessWidget {
           ),
           Padding(
             padding: const EdgeInsets.all(AppDimens.kPadding),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                GestureDetector(
-                  onTap: () => Navigator.pop(context),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(
-                      AppDimens.kPaddingXL,
-                    ),
-                    child: Container(
-                      height: 50,
-                      width: 50,
-                      color: AppColors.blackColor.withOpacity(0.5),
-                      child: const Icon(
+            child: SafeArea(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  GestureDetector(
+                    onTap: () => Navigator.pop(context),
+                    child: _borderContainer(
+                      const Icon(
                         Icons.arrow_back,
                         color: AppColors.whiteColor,
                       ),
                     ),
                   ),
-                ),
-                GestureDetector(
-                  onTap: () {
-                    Navigator.pop(context);
-                  },
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(
-                      AppDimens.kPaddingXL,
-                    ),
-                    child: Container(
-                      height: 50,
-                      width: 50,
-                      color: AppColors.blackColor.withOpacity(0.5),
-                      child: const Icon(
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                    child: _borderContainer(
+                      const Icon(
                         Icons.shopping_bag,
                         color: AppColors.whiteColor,
                       ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
           Positioned(
-            bottom: 0,
+            bottom: 20,
+            right: 10,
+            left: 10,
             child: ClipRRect(
               borderRadius: BorderRadius.circular(
                 AppDimens.kPaddingXL,
               ),
               child: Container(
-                width: size.width,
                 height: 150,
                 color: AppColors.blackColor.withOpacity(0.5),
                 child: Padding(
@@ -176,46 +187,24 @@ class CoffeeView extends StatelessWidget {
                             size: 30,
                           ),
                           Expanded(child: CustomText.h3('4.5')),
-                          Padding(
-                            padding: const EdgeInsets.all(AppDimens.kPadding),
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.circular(
-                                AppDimens.kDefaultPadding,
-                              ),
-                              child: Container(
-                                width: 40,
-                                height: 40,
-                                color: AppColors.whiteColor,
-                                child: Center(
-                                  child: Padding(
-                                    padding: const EdgeInsets.all(
-                                      AppDimens.kPadding,
-                                    ),
-                                    child: Image.asset(
-                                      'assets/icons/GraoIcon.png',
-                                    ),
-                                  ),
-                                ),
-                              ),
+                          _borderContainer(
+                            SvgPicture.asset(
+                              'assets/icons/GraoIcon.svg',
+                              height: 30,
+                              width: 30,
                             ),
+                            size: 40,
+                            color: AppColors.whiteColor,
                           ),
-                          ClipRRect(
-                            borderRadius: BorderRadius.circular(
-                              AppDimens.kDefaultPadding,
+                          _borderContainer(
+                            CustomText.h1(
+                              intensity.toString(),
+                              color: intensity >= 7
+                                  ? AppColors.redColor
+                                  : AppColors.blackColor,
                             ),
-                            child: Container(
-                              width: 40,
-                              height: 40,
-                              color: AppColors.whiteColor,
-                              child: Center(
-                                child: CustomText.h1(
-                                  intensity.toString(),
-                                  color: intensity >= 7
-                                      ? AppColors.redColor
-                                      : AppColors.blackColor,
-                                ),
-                              ),
-                            ),
+                            size: 40,
+                            color: AppColors.whiteColor,
                           ),
                         ],
                       ),
@@ -226,6 +215,27 @@ class CoffeeView extends StatelessWidget {
             ),
           ),
         ],
+      ),
+    );
+  }
+
+  Padding _borderContainer(
+    Widget child, {
+    Color color = AppColors.blackColor,
+    double size = 60.0,
+  }) {
+    return Padding(
+      padding: const EdgeInsets.all(AppDimens.kPaddingXS),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(
+          AppDimens.kDefaultPadding,
+        ),
+        child: Container(
+          height: size,
+          width: size,
+          color: color.withOpacity(0.5),
+          child: Center(child: child),
+        ),
       ),
     );
   }
