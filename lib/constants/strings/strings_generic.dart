e849cf1b79932c67b71b17ced1a877153bfd799f
Lucas <63049317+teixeiradlucas@users.noreply.github.com>
2024-01-14 09:36:40 -0300

    Novas Strings

--- lib/constants/strings/strings_generic.dart
+++ lib/constants/strings/strings_generic.dart
@@ -2,4 +2,10 @@ class AppStringsGeneric {
   static const appName = 'Cafeteria Prime';
   static const favorites = 'Favoritos';
   static const bestSellers = 'Mais vendidos';
+  static const small = 'Pequeno';
+  static const medium = 'Médio';
+  static const big = 'Grande';
+  static const addCart = 'Adicionar ao carrinho';
+  static const price = 'PREÇO';
+  static const descrition = 'Descrição';
 }
