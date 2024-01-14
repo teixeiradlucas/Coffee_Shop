b64383a2c3100d61964bf3899b37ca107a8c89e8
Lucas <63049317+teixeiradlucas@users.noreply.github.com>
2024-01-14 09:37:14 -0300

    Separando componentes

---
+++ lib/view/coffee/components/border_container.dart
@@ -0,0 +1,34 @@
+import 'package:app_coffee_shop/constants/components/app_dimension.dart';
+import 'package:app_coffee_shop/constants/themes/app_colors.dart';
+import 'package:flutter/material.dart';
+
+class BorderContainer extends StatelessWidget {
+  const BorderContainer({
+    required this.child,
+    super.key,
+    this.color = AppColors.blackColor,
+    this.size = 60.0,
+  });
+
+  final Widget child;
+  final Color color;
+  final double size;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(AppDimens.kPaddingXS),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(
+          AppDimens.kDefaultPadding,
+        ),
+        child: Container(
+          height: size,
+          width: size,
+          color: color.withOpacity(0.5),
+          child: Center(child: child),
+        ),
+      ),
+    );
+  }
+}
