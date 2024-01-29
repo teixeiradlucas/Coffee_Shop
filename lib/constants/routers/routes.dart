170e42543de39988832d13fbb08c4b7c644a1756
Lucas Francisco <lucas.francisco.teixeira7@gmail.com>
2024-01-28 23:27:17 -0300

    Criado rotas genericas para evitar padronização

--- lib/constants/routers/routes.dart
+++ lib/constants/routers/routes.dart
@@ -1,3 +1,5 @@
+import 'package:coffee_shop/constants/strings/routes_generic.dart';
+import 'package:coffee_shop/view/cart/cart_finish_view.dart';
 import 'package:coffee_shop/view/cart/cart_view.dart';
 import 'package:coffee_shop/view/coffee/coffee_view.dart';
 import 'package:coffee_shop/view/favorites/favorites_view.dart';
@@ -8,31 +10,36 @@ import 'package:go_router/go_router.dart';
 final routes = GoRouter(
   routes: [
     GoRoute(
-      name: 'Home',
-      path: '/',
+      name: RoutesGeneric.homeName,
+      path: RoutesGeneric.homeRoute,
       builder: (context, state) => const HomeView(),
     ),
     GoRoute(
-      name: 'Home Coffee',
-      path: '/home_coffee',
+      name: RoutesGeneric.homeCoffeeName,
+      path: RoutesGeneric.homeCoffeeRoute,
       builder: (context, state) => const HomeCoffeeView(),
     ),
     GoRoute(
-      name: 'Coffee View',
-      path: '/coffee_view/:id',
+      name: RoutesGeneric.coffeeViewName,
+      path: '${RoutesGeneric.coffeeViewRoute}:id',
       builder: (context, state) => CoffeeView(
         idCoffe: state.pathParameters['id']!,
       ),
     ),
     GoRoute(
-      name: 'Favorites',
-      path: '/favorites',
+      name: RoutesGeneric.favoritesName,
+      path: RoutesGeneric.favoritesRoute,
       builder: (context, state) => const FavoritesView(),
     ),
     GoRoute(
-      name: 'Cart',
-      path: '/cart',
+      name: RoutesGeneric.cartName,
+      path: RoutesGeneric.cartRoute,
       builder: (context, state) => const CartView(),
+    ),
+    GoRoute(
+      name: RoutesGeneric.cartFinishName,
+      path: RoutesGeneric.cartFinishRoute,
+      builder: (context, state) => const CartFinishView(),
     ),
   ],
 );
---
+++ lib/constants/strings/routes_generic.dart
@@ -0,0 +1,14 @@
+class RoutesGeneric {
+  static const homeName = 'Home';
+  static const homeRoute = '/';
+  static const homeCoffeeName = 'Home Coffee';
+  static const homeCoffeeRoute = '/home_coffee';
+  static const coffeeViewName = 'Coffee View';
+  static const coffeeViewRoute = '/coffee_view/';
+  static const favoritesName = 'Favorites';
+  static const favoritesRoute = '/favorites';
+  static const cartName = 'Cart';
+  static const cartRoute = '/cart';
+  static const cartFinishName = 'Cart Finish';
+  static const cartFinishRoute = '/cart_finish';
+}
