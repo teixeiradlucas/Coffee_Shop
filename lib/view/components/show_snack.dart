63d4b16711a22e05610930770ea7902d5ba6e156
Lucas Francisco Teixeira <63049317+teixeiradlucas@users.noreply.github.com>
2025-12-29 21:29:35 -0300

    Alterando show snack

--- lib/view/components/show_snack.dart
+++ lib/view/components/show_snack.dart
@@ -4,12 +4,9 @@ import 'package:flutter/material.dart';
 
 SnackBar showSnack(String text) {
   return SnackBar(
-    backgroundColor: AppColors.brownCoffeeColor,
+    backgroundColor: AppColors.blackColor.withValues(alpha: 0.5),
     content: Center(
-      child: TextCustom.h2(
-        text,
-        color: AppColors.whiteColor,
-      ),
+      child: TextCustom.h2(text, color: AppColors.whiteColor),
     ),
   );
 }
